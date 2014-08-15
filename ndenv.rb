7edfe07edc22f8fb2bb9a050dc459712e86e0f6a
WAKASUGI Mubae <baenej@gmail.com>
2014-08-15 23:15:31 +0900

    Fixed typo and needless line.

--- ndenv.rb
+++ ndenv.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
-class Nbenv < Formula
+class Ndenv < Formula
   homepage 'https://github.com/5t111111/ndenv'
   url 'https://github.com/5t111111/ndenv/archive/v0.0.0.1.tar.gz'
   sha1 '9f9404a2acc22541742ada01904a735e7bd01fcb'
 
-  head 'https://github.com/sstephenson/rbenv.git'
   head 'https://github.com/5t111111/ndenv.git'
 
   def install
